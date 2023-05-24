cfc308549a7a9c0088bca5bae0f6e91bdd6eaeae
vikiVerner <viktoriia.verner@gmail.com>
2023-05-24 11:27:12 +0200

    HashMap update

--- src/MyHashMap/MyHashMap.java
+++ src/MyHashMap/MyHashMap.java
@@ -2,105 +2,102 @@ package MyHashMap;
 
 import java.util.StringJoiner;
 
+    public class MyHashMap <K, V> {
+        // Вкладений клас, що представляє вузол мапи
+        private static class Node <K, V>{
+            private K key;
+            private V value;
+            private Node <K, V> next;
 
-public class MyHashMap<K,V> {
-    int counter;
-    private Node<K,V> firstElement;
-    private Node<K,V> lastElement;
-
-    public void put(K key, V value){
-        Node<K,V> element = new Node<K,V>();
-        element.setKey(key);
-        element.setValue(value);
-        if(firstElement==null){
-            firstElement = element;
-        }else {
-            lastElement = firstElement;
-            while (lastElement.getNext() != null) {
-               if(lastElement.getKey()!= key||key.equals(lastElement.getKey())) {
-                   lastElement = lastElement.getNext();
-               }else{
-                   throw new IllegalArgumentException("key already exist");
-               }
-            }
-            lastElement.setNext(element);
-        }
-        counter++;
-    }
-    public int size(){
-        return counter;
-    }
-
-    public V get(K key){
-        Node<K,V> search = new Node<K, V>();
-        search = firstElement;
-        V value = null;
-        for(int i=0; i<size(); i++){
-            if(search.getKey()==key||key.equals(search.getKey())){
-                value =  search.getValue();
-            }else{
-                search = search.getNext();
-            }
-        } return  value;
-    }
-
-    public void remove(K key){
-        Node<K,V> search = new Node<K, V>();
-        if(counter==0){
-            throw new NullPointerException("No existing pairs");
-        }
-        if(counter == 1){
-//            firstElement = null;
-            firstElement.setKey(null);
-            firstElement.setValue(null);
-            counter--;
-            return;
-        }
-        search = firstElement;
-        for(int i=0; i<size(); i++){
-            if(search.getKey()==key||key.equals(search.getKey())){
-                search.setValue(null);
-                search.setKey(null);
-                counter--;
-            }else{
-                search = search.getNext();
+            Node(K key, V value) {
+                this.key = key;
+                this.value = value;
             }
         }
-    }
-    public  void clear(){
-        Node<K,V> nextElement = new Node<K,V>();
-        nextElement = firstElement;
-        for(int i=0; i<size(); i++) {
-           remove(nextElement.getKey());
-           nextElement = nextElement.getNext();
+        // Константа, що визначає початковий розмір мапи
+        private static final int DEFAULT_CAPACITY = 16;
+        private Node <K, V> [] table;
+        private int size;
+
+        public MyHashMap() {
+            table = new Node[DEFAULT_CAPACITY];
+            size = 0;
         }
-    }
 
+        public void put(K key, V value) // Додає ключ та значення до мапи
+        {
+            int index = hash(key);
+            Node <K, V> node = table[index];
 
-   @Override
-   public String toString(){
-       StringJoiner result = new StringJoiner(", ");
-       Node<K,V> element = firstElement;
-       boolean a = firstElement.getKey() == null||firstElement.getKey().equals(null);
-       if(counter == 1){
-           if(!a){
-           return "{"+firstElement.getKey()+"="+ firstElement.getValue()+"}";
-           }
-           else{
-               return "[]";
-           }
-       }else{
-           if(!a) {
-               result.add("{" + element.getKey() + "=" + element.getValue() + "}");
-           }
-       }
-       while(element.getNext()!=null){
-           element = element.getNext();
-           boolean b = element.getKey() == null||(element.getKey()).equals(null);
-           if(!b) {
-               result.add("{" + element.getKey() + "=" + element.getValue() + "}");
-           }
-       }
-           return "[" + result + "]";
-       }
+            // Перевірка, чи ключ вже є в мапі
+            while (node != null) {
+                if (node.key.equals(key)) {
+                    node.value = value;
+                    return;
+                }
+                node = node.next;
+            }
+
+            // Create a new node with the key-value pair and add it to the map
+            Node <K, V> newNode = new Node(key, value);
+            newNode.next = table[index];
+            table[index] = newNode;
+            size++;
+        }
+        public void remove(K key)
+        {
+            int index = hash(key);
+            Node <K, V> node = table[index];
+
+            // Find the node with the given key and remove it
+            while (node != null) {
+                if (node.key.equals(key)) {
+                        table[index] = node.next;
+                    }
+                    size--;
+                    return;
+                }
+                node = node.next;
+
+        }
+        public void clear()
+        {
+            table = new Node[DEFAULT_CAPACITY];
+            size = 0;
+        }
+        public int size()
+        {
+            return size;
+        }
+        public V get(K key)
+        {
+            int index = hash(key);
+            Node <K, V> node = table[index];
+
+            // Find the node with the given key and return its value
+            while (node != null) {
+                if (node.key.equals(key)) {
+                    return node.value;
+                }
+                node = node.next;
+            }
+
+            // Key not found in the map
+            return null;
+        }
+        private int hash(K key) {
+            return Math.abs(key.hashCode() % table.length);
+        }
+
+        @Override
+        public String toString() {
+            StringJoiner result = new StringJoiner(", ");
+            for (Node <K, V> node : table) {
+                while (node != null) {
+                    result.add("(" + node.key.toString() + ", " + node.value.toString() + ")");
+                    node = node.next;
+                }
+            }
+            return "[" + result + "]";
+        }
 }
--- src/MyHashMap/MyHashMapTest.java
+++ src/MyHashMap/MyHashMapTest.java
@@ -1,8 +1,5 @@
 package MyHashMap;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class MyHashMapTest {
     public static void main(String[] args) {
        MyHashMap<String,Integer> m = new MyHashMap<String, Integer>();
@@ -10,10 +7,10 @@ public class MyHashMapTest {
         m.put("b",2);
         m.put("c",3);
         System.out.println("m = " + m);
-        m.remove("b");
-        System.out.println("m = " + m);
         System.out.println("m.get(\"b\") = " + m.get("b"));
         System.out.println("m.size() = " + m.size());
+        m.remove("a");
+        System.out.println("m = " + m);
         m.clear();
         System.out.println("m.size() after clear = " + m.size());
         System.out.println("m = " + m);
